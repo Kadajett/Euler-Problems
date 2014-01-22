0caae97354d5614391a359eda754c1e02882e37b
Jeremy Stover <jstover@accesso.com>
2014-01-22 16:03:06 -0500

    I have been stuck on this problem for like two days. Completely baffled as to why I was getting an incorrect value... I was logging the incrementing value, and nt the sum...

--- projectTen.js
+++ projectTen.js
@@ -3,19 +3,23 @@
 var sum = 0;
 
 
-for(var i = 1; i < 2000000; i++){
+for(var i = 2; i <= 2000000; i++){
 	if(isPrime(i)){
+		
 		sum += i;
+		
 	}
 
 }
 
-console.log(i);
+console.log(sum);
 function isPrime(number){
 	for(var i=2; i<= Math.sqrt(number); i++){
-		if(number%i ==0){
+		if(number%i == 0){
+		
 		return false;
 		}
 	}
+	
 	return true;
 }
